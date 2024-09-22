fa4d6ca1a45d7ee3c40f8895377d3e8771a3ca34
Tatheer Hussain <tatheerabidi0@gmail.com>
2024-09-22 10:58:26 +0500

    post method

--- index.mjs
+++ index.mjs
@@ -11,3 +11,8 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.post('/data', (req, res) => {
+  const data = req.body;
+  res.send(`Data received: ${JSON.stringify(data)}`);
+});
+
